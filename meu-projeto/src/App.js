7046874d23f25e72cfadcf5d850bb410b2d2c55c
matheus albuquer <mj898926@gmail.com>
2025-02-09 21:32:50 -0300

    First commit

---
+++ meu-projeto/src/App.js
@@ -0,0 +1,25 @@
+import './App.css';
+
+function App() {
+
+  const name = 'matheus';
+
+  const newName = name.toUpperCase()
+
+  function sum(a, b){
+    return a +b;
+  }
+
+  const url = 'https://via.placeholder.com/150';
+
+  return (
+    <div className="App">
+      <h2>Alterando o JSX</h2>
+      <p>Olá, {newName}</p>
+      <p>Soma {sum(1,2)}</p>
+      <img src={url} alt="Minha Imagem"/>
+    </div>
+  );
+}
+
+export default App;
