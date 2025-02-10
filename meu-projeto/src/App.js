22fcd1be8153db5e23e5e2424992b55627dc7569
matheus albuquer <mj898926@gmail.com>
2025-02-10 15:30:44 -0300

    add css styles

--- meu-projeto/src/App.js
+++ meu-projeto/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import HelloWorld from './components/HelloWorld';
 import SayMyName from './components/SayMyName';
 import Pessoa from './components/Pessoa';
+import Frase from './components/Frase';
 
 function App() {
 
@@ -9,6 +10,9 @@ function App() {
 
   return (
     <div className="App">
+      <h1>Testando css</h1>
+      <Frase/>
+      <Frase/>
       <SayMyName nome="Matheus"/>
       <SayMyName nome="João"/>
       <SayMyName nome={nome}/>
